0ea70ee9b41719267a3d21ffa2a093d22ff75efb
Ezequiel Flores <jef_artista@hotmail.com>
2022-01-18 18:51:08 -0300

    transactions model generated

---
+++ db/migrate/20220118214950_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :transactions do |t|
+      t.string :id_transaction
+      t.integer :ammount
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
